551f407f90a76eaf22c1d7270553ef3d5ad4c31b
Hundrog <reciog.oscar@hotmail.com>
2025-06-20 12:48:48 -0600

    hook list to backend

--- components/item/List.vue
+++ components/item/List.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
-const items = ref([
-    {
-        id: 1,
-        name: "Banderilla",
-        amount: 18,
-        cost: 12,
-        price: 25
-    }
-])
+import { toast } from 'vue-sonner'
+import type { Database } from '~/database.types'
+type Product = Database['public']['Tables']['products']['Row']
+const client = useSupabaseClient()
+const items = ref<Product[]>([])
+
+onMounted(async () => {
+  const { data, error } = await client
+    .from('products')
+    .select('*')
+
+  if (error) {
+    toast.error(error.message, {
+      description: 'Something went wrong while fetching the products.',
+      duration: 5000
+    })
+  } else {
+    items.value = data
+  }
+})
 </script>
 <template>
-    <ul class="list bg-base-100 rounded-box shadow-md">
+    <ul class="list bg-base-100 rounded-box shadow-md space-y-2">
 
         <!-- <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Most played songs this week</li> -->
 
@@ -20,7 +31,7 @@ const items = ref([
             <div>
                 <div class="text-xl">{{ item.name }}</div>
                 <div class="text-xs font-semibold opacity-60">{{ item.amount }} restantes</div>
-                <div class="text-xs font-semibold">Ganancia: {{ item.price - item.cost }}</div>
+                <div class="text-xs font-semibold">Ganancia: ${{ item.price - item.cost }}</div>
             </div>
             <ItemRefillButton />
             <ItemSellButton />
